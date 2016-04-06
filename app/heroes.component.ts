0990b22f03293fc3e26758e6b75f2f90f6801450
campitos <rapidclimate@gmail.com>
2016-04-06 13:17:47 -0500

    hola 'hola '

---
+++ app/heroes.component.ts
@@ -0,0 +1,35 @@
+import {Component} from 'angular2/core';
+import {HeroService} from './hero.service';
+import {HeroDetailComponent} from './hero-detail.component';
+import {OnInit} from 'angular2/core';
+import {RouteParams} from 'angular2/router';
+import {Router} from 'angular2/router';
+
+
+interface Hero {
+  id: number;
+  name: string;
+}
+@Component({
+  selector: 'my-heroes',
+  templateUrl: 'app/heroes.component.html',
+  styleUrls:  ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(
+    private _router: Router,
+    private _heroService: HeroService) { }
+  getHeroes() {
+    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+  gotoDetail() {
+    this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+  }
+}
